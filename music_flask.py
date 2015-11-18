cb8619b42d4aed2478433836042d6e5b9c243d05
David Karapetyan <david.karapetyan@gmail.com>
2015-11-18 17:06:29 -0500

    first commit

---
+++ music_flask.py
@@ -0,0 +1,32 @@
+import flask
+
+app = flask.Flask(__name__, static_folder='static', template_folder='templates')
+
+
+@app.route('/')
+def index():
+    return flask.render_template("layout.html", page_to_insert="about.html")
+
+
+@app.route('/about')
+def about():
+    return flask.render_template("layout.html", page_to_insert="about.html")
+
+
+# @app.route('/test')
+# def test():
+#     return flask.render_template("layout.html", page_to_insert="about.html")
+
+
+@app.route('/publications')
+def publications():
+    return flask.send_file("static/pdfs/publications.pdf")
+
+
+@app.route('/resume')
+def resume():
+    return flask.send_file("static/pdfs/resume.pdf")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
